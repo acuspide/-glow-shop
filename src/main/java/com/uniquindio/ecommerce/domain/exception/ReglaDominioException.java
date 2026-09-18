f78d35b2cef482bca94ecc3782c1c0b1d9eb5fb1
acuspide <alejandro.cubidesp@uqvirtual.edu.co>
2026-09-18 18:17:57 -0500

    Delete src directory

--- src/main/java/com/uniquindio/ecommerce/domain/exception/ReglaDominioException.java
+++
@@ -1,7 +0,0 @@
-package com.uniquindio.ecommerce.domain.exception;
-
-public class ReglaDominioException extends RuntimeException {
-    public ReglaDominioException(String message) {
-        super(message);
-    }
-}
