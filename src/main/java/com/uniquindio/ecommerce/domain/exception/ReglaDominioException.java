7d906e22537207565dd6a10b732b8e3d82c8ebaf
yullvanegas <yulbrynero.vanegasc@uqvirtual.edu.co>
2026-09-18 16:51:39 -0500

    Agrego carpetas de estrucuta Glow Shop

---
+++ src/main/java/com/uniquindio/ecommerce/domain/exception/ReglaDominioException.java
@@ -0,0 +1,7 @@
+package com.uniquindio.ecommerce.domain.exception;
+
+public class ReglaDominioException extends RuntimeException {
+    public ReglaDominioException(String message) {
+        super(message);
+    }
+}
